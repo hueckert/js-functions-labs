b2d3bce630ea5b5cae4ced47aa4ad03af8ee6e2c
hueckert <howard.ueckert@protonmail.com>
2024-07-16 20:02:17 -0500

    up to 11

--- script.js
+++ script.js
@@ -141,7 +141,95 @@ Example: convertTemperature(32, 'F') should return 0 (Celsius).
 
 Complete the exercise in the space below:
 */
-function convertTemperature ( temp, tempType)
-
+function convertTemperature (temp, type){
+ return ((temp *(9/5)) + 32 + (type = " Celsius"))
+}
 
 console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+function basicCalculator (num1, num2, opr){
+    if (opr === "add"){
+        return num1 + num2
+    }else if
+        (opr === "subtract")
+        return num1 - num2
+    else if
+        (opr === "multiply")
+        return num1 * num2
+    else
+        (ope === "divide")
+        return num1 / num2
+   }
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+function calculateGrade (grade){
+    if (grade >=90){
+        return "A"
+    }else if
+        (grade >= 80) 
+        return "B"
+    else if 
+        (grade >= 70)
+        return "C" 
+    else if 
+        (grade >= 60)
+        return "D" 
+    else
+        (grade < 60)
+        return "F" 
+}
+
+console.log('Exercise 10 Result:', calculateGrade(85));
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+function createUsername (firstName, lastName){
+    return (firstName = str.slice(0, 3)) + (lastName = str.slice(0, 3))+(lastName.lenth + lastName.lenth) 
+}
+
+console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
